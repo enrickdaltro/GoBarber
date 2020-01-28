56bbb9fe4e201a597b05c63d99fcb412d61f09d4
Enrick <enrickdaltro@gmail.com>
2020-01-28 19:42:46 -0300

    create a connection to postgres database

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Enrick Lins',
+        email: 'enricklins@gmail.com',
+        password_hash: '12345'
+    });
+
+    return res.json(user);
+});
 
 export default routes;
