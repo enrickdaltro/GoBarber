f937d4e7168fa53377eef74cd5ec6b73274e0c0e
Enrick <enrickdaltro@gmail.com>
2020-02-03 15:42:46 -0300

    adding email with nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '3753359a58bbf6',
+        pass: 'ea4f7c513ba863'
+    },
+    default: {
+        from: 'Equipe Gobarber <noreply@gobarber.com>'
+    }
+};
