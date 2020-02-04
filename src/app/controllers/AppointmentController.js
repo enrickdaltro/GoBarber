f1ac4bc2575288c581f2fa73d2bc4b6f6be49382
Enrick <enrickdaltro@gmail.com>
2020-02-04 11:43:19 -0300

    setting email template

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -8,6 +8,7 @@ import File from '../models/File';
 
 import Notification from '../schemas/Notification';
 import Mail from '../../lib/Mail';
+
 class AppointmentController {
     async index(req, res) {
         const { page = 1 } = req.query;
@@ -100,6 +101,11 @@ class AppointmentController {
                     model: User,
                     as: 'provider',
                     attributes: ['name', 'email']
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name']
                 }
             ]
         });
@@ -125,7 +131,18 @@ class AppointmentController {
         await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento Cancelado',
-            text: 'Você tem um novo cancelamento'
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(
+                    appointment.date,
+                    "'dia' dd 'de' MMMM', às' H:mm'h'",
+                    {
+                        locale: pt
+                    }
+                )
+            }
         });
 
         return res.json(appointment);
